c33a56dea145cee57734f253c14318cabb43623e
MariiaKalashnik <kalashnikmaria66@gmail.com>
2024-08-16 08:48:52 +0300

    feature: test cases

---
+++ tests/Feature/BasketTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BasketTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_total_1(): void
+    {
+
+        $products = ['B01', 'G01'];
+        $total = 37.85;
+
+        $this->totals($products, $total);
+    }
+
+    public function totals(array $products, float $total)
+    {
+        $this->authUser();
+
+        collect($products)->each(function ($code) use (&$response) {
+            $response = $this->postJson('api/basket-products', [
+                'code' => $code
+            ]);
+
+        }
+        );
+
+        $this->assertEquals($response->original->total, $total);
+    }
+
+    public function test_total_2(): void
+    {
+        $products = ['R01', 'R01'];
+        $total = 54.37;
+
+        $this->totals($products, $total);
+    }
+
+    public function test_total_3(): void
+    {
+        $products = ['R01', 'G01'];
+        $total = 60.85;
+
+        $this->totals($products, $total);
+    }
+
+    public function test_total_4(): void
+    {
+        $products = ['B01', 'B01', 'R01', 'R01', 'R01'];
+        $total = 98.27;
+
+        $this->totals($products, $total);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function authUser(array $fields = [])
+    {
+        $user = User::factory()->create($fields);
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
